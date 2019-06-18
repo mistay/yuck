6592ef5077cd96d76c493f14ef666fc423bf3229
armin langhofer <office@langhofer.at>
2019-06-19 00:12:55 +0200

    added result classes

---
+++ yuck/yuck/MatrixMessageResult.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yuck
+{
+    public class MatrixMessagesResult
+    {
+        public string start { get; set; }
+        public string end { get; set; }
+        public List<MatrixMessagesChunkResult> chunk { get; set; }
+    }
+
+
+    public class MatrixMessagesChunkResult
+    {
+        public string type;
+        public string room_id;
+        public string sender;
+        public MatrixMessagesContentResult content;
+        public string event_id;
+        public string user_id;
+        public long age;
+    }
+
+    public class MatrixMessagesContentResult
+    {
+        public string algorithm;
+        public string sender_key;
+        public string ciphertext;
+        public string session_id;
+        public string device_id;
+    }
+}
---
+++ yuck/yuck/MatrixSyncResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yuck
+{
+
+
+    public class MatrixSyncResult
+    {
+        public string next_batch { get; set; }
+
+        public SyncResultRooms rooms { get; set; }
+    }
+
+
+    public class SyncResultRooms
+    {
+    }
+
+}
