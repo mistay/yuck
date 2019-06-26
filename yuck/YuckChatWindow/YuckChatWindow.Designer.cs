0a0519b949c95a84edf8fea7e95851615d3f7a5e
armin langhofer <office@langhofer.at>
2019-06-26 02:36:07 +0200

    chat window init commit

---
+++ yuck/YuckChatWindow/YuckChatWindow.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace YuckChatWindow
+{
+    partial class YuckChatControl
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung. 
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // YuckChatControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.Name = "YuckChatControl";
+            this.Size = new System.Drawing.Size(830, 475);
+            this.Load += new System.EventHandler(this.UserControl1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ yuck/YuckChatWindow/YuckChatWindow.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YuckChatWindow
+{
+    public partial class YuckChatControl: UserControl
+    {
+        public int Y;
+
+        public YuckChatControl()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void AddMessage(bool ownMessage, string message)
+        {
+            Console.WriteLine("new y: " + Y);
+            TextBox l = new TextBox();
+            l.Multiline = true;
+
+            //l.Width = this.Width - 50; // max. breite bissi kleiner als window-breite
+            l.Text = message;
+
+            TextFormatFlags flags = TextFormatFlags.WordBreak;
+            Size size = TextRenderer.MeasureText(l.Text, l.Font, new Size(this.Width - 100, 100000000), flags);
+            int x = !ownMessage ? 0 : this.Width - size.Width - 30;
+            l.Location = new Point(x,Y - this.VerticalScroll.Value);
+            l.Size = size;
+            l.ForeColor = ownMessage ? Color.White : Color.Black;
+            Y += size.Height + 10;
+            l.ReadOnly = true;
+            l.BorderStyle = BorderStyle.None;
+
+            l.BackColor = ownMessage ? Color.FromArgb(0x03, 0x7C, 0xFF) : Color.LightGray;
+            l.Multiline = true;
+            this.Controls.Add(l);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            AddMessage(true, "foo\r\n\aaa");
+        }
+
+        public void AddImage(bool ownMessage, Image image, string message)
+        {
+            PictureBox p = new PictureBox();
+            p.Image = image;
+            p.Size = new Size(100, 100);
+            p.SizeMode = PictureBoxSizeMode.Zoom;
+            int x = !ownMessage ? 0 : this.Width - 100;
+            p.Location = new Point(x, Y - this.VerticalScroll.Value);
+            this.Controls.Add(p);
+
+            Y += 100;
+            TextBox l = new TextBox();
+            l.Text = message;
+            Size size = TextRenderer.MeasureText(l.Text, l.Font);
+
+            x = !ownMessage ? 0 : this.Width - size.Width - 30;
+            l.Location = new Point(x, Y - this.VerticalScroll.Value);
+            l.Size = size;
+            l.ForeColor = ownMessage ? Color.White : Color.Black;
+            Y += size.Height + 10;
+            l.ReadOnly = true;
+            l.BorderStyle = BorderStyle.None;
+
+            l.BackColor = ownMessage ? Color.FromArgb(0x03, 0x7C, 0xFF) : Color.LightGray;
+            l.Multiline = true;
+            this.Controls.Add(l);
+        }
+    }
+}
