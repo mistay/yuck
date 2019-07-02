70954b0abeb7392a9b61de7ab58bef17b678b8ba
armin langhofer <office@langhofer.at>
2019-07-02 23:36:05 +0200

    ChatMessageImage.cs init commit

---
+++ yuck/yuck/ChatMessageImage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yuck
+{
+    internal class ChatMessageImage : ChatMessage
+    {
+        public string Url {get; set;}
+        public ChatMessageImage(string roomID, string sender, string message, string url) : base(roomID, sender, message)
+        {
+            Url = url;
+        }
+    }
+}
