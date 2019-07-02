0afa89ac4ca9138b50a7d5416e026a3b61818566
armin langhofer <office@langhofer.at>
2019-07-02 18:06:52 +0200

    ChatMessage.cs: init commit

---
+++ yuck/yuck/ChatMessage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace yuck
+{
+    public class ChatMessage
+    {
+        public string Message { get; set; }
+        public string Sender { get; set; }
+        public bool Displayed { get; set; }
+        public string RoomID { get; set; }
+        public DateTime timeReceived { get; set; }
+
+        private ChatMessage()
+        {
+
+        }
+
+        public ChatMessage(string roomID, string sender, string message)
+        {
+            RoomID = roomID;
+            Sender = sender;
+            Message = message;
+            Displayed = false;
+        }
+    }
+}
