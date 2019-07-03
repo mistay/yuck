0d6f8bec0d5c9efe97dc0c23ae87357e08c8bb2e
armin langhofer <office@langhofer.at>
2019-07-04 01:39:47 +0200

    UserTypingInRoom.cs: init commit

---
+++ yuck/yuck/UserTypingInRoom.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yuck
+{
+    public class UserTypingInRoom
+    {
+        public string UserID {get; set; }
+        public string RoomID {get; set; }
+
+        private UserTypingInRoom()
+        {
+
+        }
+
+        public UserTypingInRoom(string room_id, string user_id)
+        {
+            UserID = user_id;
+            RoomID = room_id;
+        }
+    }
+}
